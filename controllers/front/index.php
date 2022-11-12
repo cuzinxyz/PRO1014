bf17fe6063abb211e066d4a468b057d3a4ca3797
Cuzin Pro <67429817+cuzinxyz@users.noreply.github.com>
2022-11-12 17:50:12 +0700

    update khung 12/11

---
+++ controllers/front/index.php
@@ -0,0 +1,3 @@
+<?php
+
+require "views/front/index.php";
--- index.php
+++ index.php
@@ -1,7 +1,22 @@
 <?php
 
-if (isset($_GET['view'])) :
-    require 'views/front/blog-detail.html';
+$action = isset($_GET['action']) ? $_GET['action'] : '';
+
+if (isset($action)) :
+    switch ($action) {
+        case 'index':
+            require "controllers/front/index.php";
+            break;
+
+        default:
+            require "views/front/404.php";
+            break;
+    }
 else :
+<<<<<<< Updated upstream
     require 'views/front/index.html';
 endif;
+=======
+    require "controllers/front/index.php";
+endif;
+>>>>>>> Stashed changes
