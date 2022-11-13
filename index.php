b600a691249cb6c2d80f546ef37c37aba3c71e7a
HaiDuong003 <102920748+HaiDuong003@users.noreply.github.com>
2022-11-13 23:08:41 +0700

    addemployee

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 
+<<<<<<< Updated upstream
 switch ($variable) {
     case 'value':
         # code...
@@ -9,3 +10,28 @@ switch ($variable) {
         # code...
         break;
 }
+=======
+$action = isset($_GET['action']) ? $_GET['action'] : null;
+
+if (isset($action)) :
+    switch ($action) {
+        case 'index':
+            require "controllers/front/index.php";
+            break;
+        case 'dashboard':
+            require "controllers/admin/index.php";
+            break;
+        case 'receipt':
+            require "controllers/admin/receipt.php";
+            break;
+        case 'addemployee':
+            require "controllers/admin/add-employee.php";
+            break;
+        default:
+            require "views/front/404.php";
+            break;
+    }
+else :
+    require "controllers/front/index.php";
+endif;
+>>>>>>> Stashed changes
