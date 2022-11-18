f54236eca1541511cabf0b21dc70856ef6da1449
tuannv <nguyenvantuan18112003dt@gmail.com>
2022-11-18 22:25:28 +0700

    update category services

--- views/admin/services.php
+++ views/admin/services.php
@@ -5,12 +5,12 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1>DataTables</h1>
+            <h1>Danh sách dịch vụ</h1>
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">DataTables</li>
+              <li class="breadcrumb-item active">Danh sách dịch vụ</li>
             </ol>
           </div>
         </div>
@@ -23,9 +23,6 @@
         <div class="row">
           <div class="col-12">
             <div class="card">
-              <div class="card-header">
-                <h3 class="card-title">DataTable with minimal features & hover style</h3>
-              </div>
               <!-- /.card-header -->
               <div class="card-body">
                 <table id="example2" class="table table-bordered table-hover">
