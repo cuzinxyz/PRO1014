4533e1da3c8a29cc203de45544995eb59ed90949
HaiDuong003 <102920748+HaiDuong003@users.noreply.github.com>
2022-11-21 13:44:52 +0700

    update service and (employee v1)

--- controllers/admin/edit_service.php
+++ controllers/admin/edit_service.php
@@ -2,16 +2,16 @@
 require "models/admin.php";
 
 $id = $_GET['id'];
-$values = categories();
+// $values = categories();
 
 $service = one_service($id);
 
 if (isset($_POST['add'])) {
   $name_service = $_POST['nameService'];
   $price_service = $_POST['priceService'];
-  $cate_service = $_POST['cateService'];
+  // $cate_service = $_POST['cateService'];
   $status = $_POST['status'];
-  update_service($name_service, $price_service, $cate_service, $status, $id);
+  update_service($name_service, $price_service, $status, $id);
   echo ("<script>location.href = '?action=services';</script>");
 }
 
