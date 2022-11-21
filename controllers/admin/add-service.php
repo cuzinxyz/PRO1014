92ae7d17d35b4d5e3bde5545485867e3e65a3bf5
tuannv <nguyenvantuan18112003dt@gmail.com>
2022-11-21 13:27:31 +0700

    delete category

--- controllers/admin/add-service.php
+++ controllers/admin/add-service.php
@@ -1,13 +1,11 @@
 <?php
 require "models/admin.php";
 
-    $values = categories();
 
 if (isset($_POST['add'])) {
     $name_service = $_POST['nameService'];
     $price_service = $_POST['priceService'];
-    $cate_service = $_POST['cateService'];
-    add_service($name_service, $price_service, $cate_service);
+    add_service($name_service, $price_service);
     echo("<script>location.href = '?action=services';</script>");
 }
 
